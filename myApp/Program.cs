7b6521d5d401d1f83d44c69ba392f6de81e840fa
hungvx <vuxuanhungmta@gmail.com>
2021-09-30 23:13:03 +0700

    commit

---
+++ myApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is current time " + DateTime.Now);
+Console.WriteLine($"This is current time {DateTime.Now}");
+Console.WriteLine("This is current time {0}", DateTime.Now);
